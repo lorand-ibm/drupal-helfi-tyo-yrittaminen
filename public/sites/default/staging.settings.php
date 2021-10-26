c5564d4bc4b391e691aaf35be4e3b554c01d3cd1
tuutti <tuutti@iki.fi>
2021-10-26 07:30:28 +0300

    Initial config

---
+++ public/sites/default/staging.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['helfi_proxy.settings']['asset_path'] = 'staging-elo-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'staging-elo-en',
+  'fi' => 'staging-elo-fi',
+  'sv' => 'staging-elo-sv',
+  'ru' => 'staging-elo-ru',
+];
---
+++ public/sites/default/testing.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['helfi_proxy.settings']['asset_path'] = 'test-elo-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'test-elo-en',
+  'fi' => 'test-elo-fi',
+  'sv' => 'test-elo-sv',
+  'ru' => 'test-elo-ru',
+];
