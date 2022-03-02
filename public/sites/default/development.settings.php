dbd1ae834714b7683a2382e986773cf3cf756a3b
tuutti <tuutti@iki.fi>
2022-03-02 07:16:55 +0200

    UHF-4437: Tunnistamo return urls

--- public/sites/default/development.settings.php
+++ public/sites/default/development.settings.php
@@ -1,5 +1,6 @@
 <?php
 
+$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/dev-yritykset-ja-tyo/openid-connect/tunnistamo';
 $config['helfi_proxy.settings']['asset_path'] = 'dev-elo-assets';
 $config['helfi_proxy.settings']['prefixes'] = [
   'en' => 'dev-business-and-work',
